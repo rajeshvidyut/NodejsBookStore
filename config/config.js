ea4174500136e3ad50be1bdb8266e2318143103b
rajeshvidyut <33810869+rajeshvidyut@users.noreply.github.com>
2019-11-21 12:02:10 +0530

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports={
+  "facebook_api_key"      :     "Your key",
+  "facebook_api_secret"   :     "Your secret code",
+  "callback_url"          :     "http://localhost:2000/auth/facebook/callback",
+  "use_database"          :      true
+}
